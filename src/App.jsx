a72510318f538617c8ef2919efcb1c09d4a6c1d3
Marcelearn <jaworskimarcel@gmail.com>
2022-10-28 11:12:40 +0200

    minimized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+    <h1>Bike Rental App</h1>
+    <p>this is it.</p>
+    </div>
+  )
+}
+
+export default App
